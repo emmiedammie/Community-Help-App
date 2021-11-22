0a8c15590113bc6c57d5fb6c46aa3adf55486db0
Damilola Olayemi <olayemi_olabanji@yahoo.com>
2021-11-22 12:35:34 +0000

    Updatedd

---
+++ Community-Help-App/Delivery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community_Help_App
+{
+    class Delivery
+    {
+        public string DeliveryDate { get; set; }
+        public string CustID { get; set; }
+        public string VolunteerID { get; set; }
+        public string AmountDue { get; set; }
+
+        public override string ToString()
+        {
+            return DeliveryDate + ", " + CustID + ", " + VolunteerID + ", " + AmountDue;
+        }
+    }
+}
