431d06be9603e85350f05115a108709803eb2489
Damilola Olayemi <olayemi_olabanji@yahoo.com>
2021-11-22 11:58:10 +0000

    Updated

---
+++ Community-Help-App/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community_Help_App
+{
+    class Order
+    {
+        public string GroceriesItems { get; set; }
+        public string StoreName { get; set; }
+        public string MedicalList { get; set; }
+        public string DeliveryDate { get; set; }
+        public string TotalOrderAmount { get; set; }
+
+        public override string ToString()
+        {
+            return GroceriesItems + ", " + StoreName + ", " + MedicalList + ", " + DeliveryDate + ", " + TotalOrderAmount; 
+
+        }
+        
+    }
+}
