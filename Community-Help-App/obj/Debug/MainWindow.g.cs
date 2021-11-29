8d1a867501d1f7dffadb48a1f94717091f98821a
Damilola Olayemi <olayemi_olabanji@yahoo.com>
2021-11-29 00:13:09 +0000

    updated

--- Community-Help-App/obj/Debug/MainWindow.g.cs
+++ Community-Help-App/obj/Debug/MainWindow.g.cs
@@ -40,6 +40,14 @@ namespace Community_Help_App {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 10 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.ListView PlaceOrder;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +76,12 @@ namespace Community_Help_App {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.PlaceOrder = ((System.Windows.Controls.ListView)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
