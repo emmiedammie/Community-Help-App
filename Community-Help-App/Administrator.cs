d405ef13a1af3cefba372abd3913c24a982ecd34
Damilola Olayemi <olayemi_olabanji@yahoo.com>
2021-12-03 07:34:46 +0000

    Updated

---
+++ Community-Help-App/Administrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community_Help_App
+{
+    class Administrator
+    {
+        public string AdminID { get; set; }
+        public string AdminUserName { get; set; }
+        public string AdminPassword { get; set; }
+      
+
+        public override string ToString()
+        {
+            return AdminID + "Welcome ";
+        }
+    }
+}
